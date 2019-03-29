8b0addce0ff98f12e717b527daf679bb20a461f8
tregubovand <tregubovand@gmail.com>
2019-03-29 21:49:14 +0300

    Property list added

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-module.exports = {
-    init: function() {
-        console.log('Pure slider inited');
-        console.log('Pure slider inited');
+function PureRangeSlider(selector, props) {
+    this.containerClass = 'js-pure-range-container';
+    this.lineClass = "js-pure-range-line";
+    this.sliderClass = "js-pure-range-slider";
+    this.start = 0;
+    this.finish = 100;
+    this.values = false;
+    this.step = 1;
+    this.selector = false;
+
+    if (!(this instanceof PureRangeSlider)) {
+        return new PureRangeSlider(selector, props = {});
     }
-}
+
+    this.elem = document.querySelector(selector);
+};
+
+
+PureRangeSlider.prototype.init = function init() {
+
+};
+
+module.exports = PureRangeSlider;
