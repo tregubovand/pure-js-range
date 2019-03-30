5acde82acbfcaad7132c74c1d7cc7889c8c7f4dd
tregubovand <tregubovand@gmail.com>
2019-03-30 17:46:12 +0300

    Default styling function

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 function PureRangeSlider(selector, props) {
     this.containerClass = 'js-pure-range-container';
     this.lineClass = "js-pure-range-line";
@@ -10,35 +8,7 @@ function PureRangeSlider(selector, props) {
     this.step = 1;
     this.selector = false;
 
-    this.lineStyle = {
-        position: 'absolute',
-        width: '100%',
-        height: '2px',
-        backgroundColor: '#333',
-        top: '50%',
-        transform: 'translate(-50%, -50%)'
-    };
-    this.containerStyle = {
-        position: 'relative',
-        height: '40px',
-        width: '100%'
-    };
-    this.sliderStyle = {
-        position: 'absolute',
-        height: '20px',
-        top: '50%',
-        backgroundColor: '#3c3d8f',
-        transform: 'translateY(-50%)',
-        cursor: 'pointer'
-    };
-    this.stepStyle = {
-        width: '2px',
-        height: '10px',
-        position: 'absolute',
-        backgroundColor: '#380f4d',
-        top: '50%',
-        transform: 'translate(50%,-50%)',
-    };
+
 
     if (!(this instanceof PureRangeSlider)) {
         return new PureRangeSlider(selector, props = {});
@@ -51,13 +21,10 @@ function PureRangeSlider(selector, props) {
     this.createSlider = function(){
         this.line = document.createElement('div');
         this.line.classList.add(this.lineClass);
-        Object.assign(this.line.style,this.lineStyle);
-
         this.elem.append(this.line);
 
         this.slider = document.createElement('div');
         this.slider.classList.add(this.sliderClass);
-        Object.assign(this.slider.style,this.sliderStyle);
 
         if(!this.values){
             this.slider.style.width = (this.finish - this.start) / 100 + '%';
@@ -68,12 +35,10 @@ function PureRangeSlider(selector, props) {
         this.elem.append(this.slider);
 
         this.elem.classList.add(this.containerClass);
-        Object.assign(this.elem, this.containerStyle);
-
+        this.defaultStyles();
         this.createStops();
         this.initSliding();
     };
-
     this.createStops = function(){
         if(!this.values){
             for(let i = this.start; i <= this.finish; i+=this.step){
@@ -98,6 +63,41 @@ function PureRangeSlider(selector, props) {
             }
         };
     }
+    this.defaultStyles = function(){
+        this.lineStyle = {
+            position: 'absolute',
+            width: '100%',
+            height: '2px',
+            backgroundColor: '#333',
+            top: '50%',
+            transform: 'translate(-50%, -50%)'
+        };
+        this.containerStyle = {
+            position: 'relative',
+            height: '40px',
+            width: '100%'
+        };
+        this.sliderStyle = {
+            position: 'absolute',
+            height: '20px',
+            top: '50%',
+            backgroundColor: '#3c3d8f',
+            transform: 'translateY(-50%)',
+            cursor: 'pointer'
+        };
+        this.stepStyle = {
+            width: '2px',
+            height: '10px',
+            position: 'absolute',
+            backgroundColor: '#380f4d',
+            top: '50%',
+            transform: 'translate(50%,-50%)',
+        };
+
+        Object.assign(this.line.style,this.lineStyle);
+        Object.assign(this.slider.style,this.sliderStyle);
+        Object.assign(this.elem.style, this.containerStyle);
+    };
 
 
     this.createSlider();
